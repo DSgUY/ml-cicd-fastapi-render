e49961a8590e85c060c62d9fe764efe77425b167
DSgUY <kenji.nakasone@correo.ucu.edu.uy>
2023-02-06 19:11:44 -0300

    added 2 tests to model

--- test_cases/test_train_model.py
+++ test_cases/test_train_model.py
@@ -1,7 +1,17 @@
 import os
-
+from sklearn.utils.validation import check_is_fitted
+from starter.ml.model import load_model
 
 def test_train_files():
     assert os.path.isfile(os.path.join('model', 'encoder_dtc.pkl'))
     assert os.path.isfile(os.path.join('model', 'lb_dtc.pkl'))
     assert os.path.isfile(os.path.join('model', 'model_dtc.pkl'))
+ 
+
+def test_load_model():
+    assert load_model(os.path.join('model', 'model_dtc.pkl'))
+
+
+def test_model():
+    model = load_model(os.path.join('model', 'model_dtc.pkl'))
+    assert not check_is_fitted(model)
